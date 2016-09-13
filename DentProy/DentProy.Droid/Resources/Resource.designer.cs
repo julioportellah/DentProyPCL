724f632de40a19e390fba7d179d6acd1ddf203bd
Julio Portella <julioportellah@gmail.com>
2016-09-13 00:51:38 -0400

    Se creo la version 0 del odontograma

--- DentProy/DentProy.Droid/Resources/Resource.designer.cs
+++ DentProy/DentProy.Droid/Resources/Resource.designer.cs
@@ -64,7 +64,31 @@ namespace DentProy.Droid
 			public const int myButton = 2131034112;
 			
 			// aapt resource value: 0x7f050001
-			public const int textView1 = 2131034113;
+			public const int name = 2131034113;
+			
+			// aapt resource value: 0x7f050009
+			public const int pieza11 = 2131034121;
+			
+			// aapt resource value: 0x7f050008
+			public const int pieza12 = 2131034120;
+			
+			// aapt resource value: 0x7f050007
+			public const int pieza13 = 2131034119;
+			
+			// aapt resource value: 0x7f050006
+			public const int pieza14 = 2131034118;
+			
+			// aapt resource value: 0x7f050005
+			public const int pieza15 = 2131034117;
+			
+			// aapt resource value: 0x7f050004
+			public const int pieza16 = 2131034116;
+			
+			// aapt resource value: 0x7f050003
+			public const int pieza17 = 2131034115;
+			
+			// aapt resource value: 0x7f050002
+			public const int pieza18 = 2131034114;
 			
 			static Id()
 			{
