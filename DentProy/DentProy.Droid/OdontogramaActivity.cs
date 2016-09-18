12bab9c2bfed14d2e91e7af849eea21b20eac2c7
Julio Portella <julioportellah@gmail.com>
2016-09-18 14:08:32 -0400

    Se agrego la interfaz de botones y se està probando el handler

--- DentProy/DentProy.Droid/OdontogramaActivity.cs
+++ DentProy/DentProy.Droid/OdontogramaActivity.cs
@@ -14,7 +14,7 @@ using DentProy.BusinessLayer;
 namespace DentProy.Droid
 {
     [Activity(Label = "OdontogramaActivity")]
-    public class OdontogramaActivity : Activity
+    public class OdontogramaActivity : Activity, View.IOnClickListener
     {
         protected override void OnCreate(Bundle savedInstanceState)
         {
@@ -23,6 +23,81 @@ namespace DentProy.Droid
             // Create your application here
             SetContentView(Resource.Layout.Odontograma);
             var pieza11 = new Pieza(11);
+            //Declarando botones de la parte superior    
+            var btnPieza11 = (Button)FindViewById(Resource.Id.pieza11);
+            var btnPieza12 = (Button)FindViewById(Resource.Id.pieza12);
+            var btnPieza13 = (Button)FindViewById(Resource.Id.pieza13);
+            var btnPieza14 = (Button)FindViewById(Resource.Id.pieza14);
+            var btnPieza15 = (Button)FindViewById(Resource.Id.pieza15);
+            var btnPieza16 = (Button)FindViewById(Resource.Id.pieza16);
+            var btnPieza17 = (Button)FindViewById(Resource.Id.pieza17);
+            var btnPieza18 = (Button)FindViewById(Resource.Id.pieza18);
+            var btnPieza21 = (Button)FindViewById(Resource.Id.pieza21);
+            var btnPieza22 = (Button)FindViewById(Resource.Id.pieza22);
+            var btnPieza23 = (Button)FindViewById(Resource.Id.pieza23);
+            var btnPieza24 = (Button)FindViewById(Resource.Id.pieza24);
+            var btnPieza25 = (Button)FindViewById(Resource.Id.pieza25);
+            var btnPieza26 = (Button)FindViewById(Resource.Id.pieza26);
+            var btnPieza27 = (Button)FindViewById(Resource.Id.pieza27);
+            var btnPieza28 = (Button)FindViewById(Resource.Id.pieza28);
+            var btnPieza31 = (Button)FindViewById(Resource.Id.pieza31);
+            var btnPieza32 = (Button)FindViewById(Resource.Id.pieza32);
+            var btnPieza33 = (Button)FindViewById(Resource.Id.pieza33);
+            var btnPieza34 = (Button)FindViewById(Resource.Id.pieza34);
+            var btnPieza35 = (Button)FindViewById(Resource.Id.pieza35);
+            var btnPieza36 = (Button)FindViewById(Resource.Id.pieza36);
+            var btnPieza37 = (Button)FindViewById(Resource.Id.pieza37);
+            var btnPieza38 = (Button)FindViewById(Resource.Id.pieza38);
+            var btnPieza41 = (Button)FindViewById(Resource.Id.pieza41);
+            var btnPieza42 = (Button)FindViewById(Resource.Id.pieza42);
+            var btnPieza43 = (Button)FindViewById(Resource.Id.pieza43);
+            var btnPieza44 = (Button)FindViewById(Resource.Id.pieza44);
+            var btnPieza45 = (Button)FindViewById(Resource.Id.pieza45);
+            var btnPieza46 = (Button)FindViewById(Resource.Id.pieza46);
+            var btnPieza47 = (Button)FindViewById(Resource.Id.pieza47);
+            var btnPieza48 = (Button)FindViewById(Resource.Id.pieza48);
+
+            btnPieza11.SetOnClickListener(this);
+            btnPieza12.SetOnClickListener(this);
+            btnPieza13.SetOnClickListener(this);
+            btnPieza14.SetOnClickListener(this);
+            btnPieza15.SetOnClickListener(this);
+            btnPieza16.SetOnClickListener(this);
+            btnPieza17.SetOnClickListener(this);
+            btnPieza18.SetOnClickListener(this);
+            btnPieza21.SetOnClickListener(this);
+            btnPieza22.SetOnClickListener(this);
+            btnPieza23.SetOnClickListener(this);
+            btnPieza24.SetOnClickListener(this);
+            btnPieza25.SetOnClickListener(this);
+            btnPieza26.SetOnClickListener(this);
+            btnPieza27.SetOnClickListener(this);
+            btnPieza28.SetOnClickListener(this);
+            btnPieza31.SetOnClickListener(this);
+            btnPieza32.SetOnClickListener(this);
+            btnPieza33.SetOnClickListener(this);
+            btnPieza34.SetOnClickListener(this);
+            btnPieza35.SetOnClickListener(this);
+            btnPieza36.SetOnClickListener(this);
+            btnPieza37.SetOnClickListener(this);
+            btnPieza38.SetOnClickListener(this);
+            btnPieza41.SetOnClickListener(this);
+            btnPieza42.SetOnClickListener(this);
+            btnPieza43.SetOnClickListener(this);
+            btnPieza44.SetOnClickListener(this);
+            btnPieza45.SetOnClickListener(this);
+            btnPieza46.SetOnClickListener(this);
+            btnPieza47.SetOnClickListener(this);
+            btnPieza48.SetOnClickListener(this);
+        }
+        
+
+        public void OnClick(View v)
+        {
+            Toast toast = Toast.MakeText(this, "You Clicked the button..." + v.Id.ToString(), ToastLength.Short);
+            switch (v.Id) {
+
+            }
         }
     }
 }
--- DentProy/DentProy.Droid/Resources/Resource.designer.cs
+++ DentProy/DentProy.Droid/Resources/Resource.designer.cs
@@ -60,35 +60,209 @@ namespace DentProy.Droid
 		public partial class Id
 		{
 			
+			// aapt resource value: 0x7f050013
+			public const int blankspace1 = 2131034131;
+			
+			// aapt resource value: 0x7f050034
+			public const int blankspace2 = 2131034164;
+			
+			// aapt resource value: 0x7f05001b
+			public const int espacio1121 = 2131034139;
+			
+			// aapt resource value: 0x7f05001a
+			public const int espacio1211 = 2131034138;
+			
+			// aapt resource value: 0x7f050019
+			public const int espacio1312 = 2131034137;
+			
+			// aapt resource value: 0x7f050018
+			public const int espacio1413 = 2131034136;
+			
+			// aapt resource value: 0x7f050017
+			public const int espacio1514 = 2131034135;
+			
+			// aapt resource value: 0x7f050016
+			public const int espacio1615 = 2131034134;
+			
+			// aapt resource value: 0x7f050015
+			public const int espacio1716 = 2131034133;
+			
+			// aapt resource value: 0x7f050014
+			public const int espacio1817 = 2131034132;
+			
+			// aapt resource value: 0x7f05001c
+			public const int espacio2122 = 2131034140;
+			
+			// aapt resource value: 0x7f05001d
+			public const int espacio2223 = 2131034141;
+			
+			// aapt resource value: 0x7f05001e
+			public const int espacio2324 = 2131034142;
+			
+			// aapt resource value: 0x7f05001f
+			public const int espacio2425 = 2131034143;
+			
+			// aapt resource value: 0x7f050020
+			public const int espacio2526 = 2131034144;
+			
+			// aapt resource value: 0x7f050021
+			public const int espacio2627 = 2131034145;
+			
+			// aapt resource value: 0x7f050022
+			public const int espacio2728 = 2131034146;
+			
+			// aapt resource value: 0x7f05003c
+			public const int espacio3141 = 2131034172;
+			
+			// aapt resource value: 0x7f05003b
+			public const int espacio3231 = 2131034171;
+			
+			// aapt resource value: 0x7f05003a
+			public const int espacio3332 = 2131034170;
+			
+			// aapt resource value: 0x7f050039
+			public const int espacio3433 = 2131034169;
+			
+			// aapt resource value: 0x7f050038
+			public const int espacio3534 = 2131034168;
+			
+			// aapt resource value: 0x7f050037
+			public const int espacio3635 = 2131034167;
+			
+			// aapt resource value: 0x7f050036
+			public const int espacio3736 = 2131034166;
+			
+			// aapt resource value: 0x7f050035
+			public const int espacio3837 = 2131034165;
+			
+			// aapt resource value: 0x7f05003d
+			public const int espacio4142 = 2131034173;
+			
+			// aapt resource value: 0x7f05003e
+			public const int espacio4243 = 2131034174;
+			
+			// aapt resource value: 0x7f05003f
+			public const int espacio4344 = 2131034175;
+			
+			// aapt resource value: 0x7f050040
+			public const int espacio4445 = 2131034176;
+			
+			// aapt resource value: 0x7f050041
+			public const int espacio4546 = 2131034177;
+			
+			// aapt resource value: 0x7f050042
+			public const int espacio4647 = 2131034178;
+			
+			// aapt resource value: 0x7f050043
+			public const int espacio4748 = 2131034179;
+			
+			// aapt resource value: 0x7f050001
+			public const int mainLayer = 2131034113;
+			
 			// aapt resource value: 0x7f050000
 			public const int myButton = 2131034112;
 			
-			// aapt resource value: 0x7f050001
-			public const int name = 2131034113;
+			// aapt resource value: 0x7f05000a
+			public const int pieza11 = 2131034122;
 			
 			// aapt resource value: 0x7f050009
-			public const int pieza11 = 2131034121;
+			public const int pieza12 = 2131034121;
 			
 			// aapt resource value: 0x7f050008
-			public const int pieza12 = 2131034120;
+			public const int pieza13 = 2131034120;
 			
 			// aapt resource value: 0x7f050007
-			public const int pieza13 = 2131034119;
+			public const int pieza14 = 2131034119;
 			
 			// aapt resource value: 0x7f050006
-			public const int pieza14 = 2131034118;
+			public const int pieza15 = 2131034118;
 			
 			// aapt resource value: 0x7f050005
-			public const int pieza15 = 2131034117;
+			public const int pieza16 = 2131034117;
 			
 			// aapt resource value: 0x7f050004
-			public const int pieza16 = 2131034116;
+			public const int pieza17 = 2131034116;
 			
 			// aapt resource value: 0x7f050003
-			public const int pieza17 = 2131034115;
+			public const int pieza18 = 2131034115;
+			
+			// aapt resource value: 0x7f05000b
+			public const int pieza21 = 2131034123;
+			
+			// aapt resource value: 0x7f05000c
+			public const int pieza22 = 2131034124;
+			
+			// aapt resource value: 0x7f05000d
+			public const int pieza23 = 2131034125;
+			
+			// aapt resource value: 0x7f05000e
+			public const int pieza24 = 2131034126;
+			
+			// aapt resource value: 0x7f05000f
+			public const int pieza25 = 2131034127;
+			
+			// aapt resource value: 0x7f050010
+			public const int pieza26 = 2131034128;
+			
+			// aapt resource value: 0x7f050011
+			public const int pieza27 = 2131034129;
+			
+			// aapt resource value: 0x7f050012
+			public const int pieza28 = 2131034130;
+			
+			// aapt resource value: 0x7f05002b
+			public const int pieza31 = 2131034155;
+			
+			// aapt resource value: 0x7f05002a
+			public const int pieza32 = 2131034154;
+			
+			// aapt resource value: 0x7f050029
+			public const int pieza33 = 2131034153;
+			
+			// aapt resource value: 0x7f050028
+			public const int pieza34 = 2131034152;
+			
+			// aapt resource value: 0x7f050027
+			public const int pieza35 = 2131034151;
+			
+			// aapt resource value: 0x7f050026
+			public const int pieza36 = 2131034150;
+			
+			// aapt resource value: 0x7f050025
+			public const int pieza37 = 2131034149;
+			
+			// aapt resource value: 0x7f050024
+			public const int pieza38 = 2131034148;
+			
+			// aapt resource value: 0x7f05002c
+			public const int pieza41 = 2131034156;
+			
+			// aapt resource value: 0x7f05002d
+			public const int pieza42 = 2131034157;
+			
+			// aapt resource value: 0x7f05002e
+			public const int pieza43 = 2131034158;
+			
+			// aapt resource value: 0x7f05002f
+			public const int pieza44 = 2131034159;
+			
+			// aapt resource value: 0x7f050030
+			public const int pieza45 = 2131034160;
+			
+			// aapt resource value: 0x7f050031
+			public const int pieza46 = 2131034161;
+			
+			// aapt resource value: 0x7f050032
+			public const int pieza47 = 2131034162;
+			
+			// aapt resource value: 0x7f050033
+			public const int pieza48 = 2131034163;
+			
+			// aapt resource value: 0x7f050023
+			public const int underJaw = 2131034147;
 			
 			// aapt resource value: 0x7f050002
-			public const int pieza18 = 2131034114;
+			public const int upperJaw = 2131034114;
 			
 			static Id()
 			{
