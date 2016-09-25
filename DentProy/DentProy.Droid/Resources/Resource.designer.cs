c100dd87de5f3d79479132866ccfe4f800f3431d
Julio Portella <julioportellah@gmail.com>
2016-09-24 23:32:16 -0500

    version parcial del odontograma

--- DentProy/DentProy.Droid/Resources/Resource.designer.cs
+++ DentProy/DentProy.Droid/Resources/Resource.designer.cs
@@ -63,14 +63,20 @@ namespace DentProy.Droid
 			// aapt resource value: 0x7f050016
 			public const int blankspace1 = 2131034134;
 			
-			// aapt resource value: 0x7f050055
-			public const int blankspace2 = 2131034197;
+			// aapt resource value: 0x7f050057
+			public const int blankspace2 = 2131034199;
 			
-			// aapt resource value: 0x7f050032
-			public const int blankspace3 = 2131034162;
+			// aapt resource value: 0x7f050033
+			public const int blankspace3 = 2131034163;
 			
-			// aapt resource value: 0x7f05003e
-			public const int blankspace4 = 2131034174;
+			// aapt resource value: 0x7f05003f
+			public const int blankspace4 = 2131034175;
+			
+			// aapt resource value: 0x7f05004a
+			public const int blankspaceDienteLecheInf = 2131034186;
+			
+			// aapt resource value: 0x7f050028
+			public const int blankspaceDienteLecheSup = 2131034152;
 			
 			// aapt resource value: 0x7f05001e
 			public const int espacio1121 = 2131034142;
@@ -117,113 +123,113 @@ namespace DentProy.Droid
 			// aapt resource value: 0x7f050025
 			public const int espacio2728 = 2131034149;
 			
-			// aapt resource value: 0x7f05005e
-			public const int espacio3132 = 2131034206;
-			
-			// aapt resource value: 0x7f05005f
-			public const int espacio3233 = 2131034207;
-			
 			// aapt resource value: 0x7f050060
-			public const int espacio3334 = 2131034208;
+			public const int espacio3132 = 2131034208;
 			
 			// aapt resource value: 0x7f050061
-			public const int espacio3435 = 2131034209;
+			public const int espacio3233 = 2131034209;
 			
 			// aapt resource value: 0x7f050062
-			public const int espacio3536 = 2131034210;
+			public const int espacio3334 = 2131034210;
 			
 			// aapt resource value: 0x7f050063
-			public const int espacio3637 = 2131034211;
+			public const int espacio3435 = 2131034211;
 			
 			// aapt resource value: 0x7f050064
-			public const int espacio3738 = 2131034212;
+			public const int espacio3536 = 2131034212;
+			
+			// aapt resource value: 0x7f050065
+			public const int espacio3637 = 2131034213;
+			
+			// aapt resource value: 0x7f050066
+			public const int espacio3738 = 2131034214;
+			
+			// aapt resource value: 0x7f05005f
+			public const int espacio4131 = 2131034207;
+			
+			// aapt resource value: 0x7f05005e
+			public const int espacio4241 = 2131034206;
 			
 			// aapt resource value: 0x7f05005d
-			public const int espacio4131 = 2131034205;
+			public const int espacio4342 = 2131034205;
 			
 			// aapt resource value: 0x7f05005c
-			public const int espacio4241 = 2131034204;
+			public const int espacio4443 = 2131034204;
 			
 			// aapt resource value: 0x7f05005b
-			public const int espacio4342 = 2131034203;
+			public const int espacio4544 = 2131034203;
 			
 			// aapt resource value: 0x7f05005a
-			public const int espacio4443 = 2131034202;
+			public const int espacio4645 = 2131034202;
 			
 			// aapt resource value: 0x7f050059
-			public const int espacio4544 = 2131034201;
+			public const int espacio4746 = 2131034201;
 			
 			// aapt resource value: 0x7f050058
-			public const int espacio4645 = 2131034200;
-			
-			// aapt resource value: 0x7f050057
-			public const int espacio4746 = 2131034199;
-			
-			// aapt resource value: 0x7f050056
-			public const int espacio4847 = 2131034198;
-			
-			// aapt resource value: 0x7f050037
-			public const int espacio5161 = 2131034167;
-			
-			// aapt resource value: 0x7f050036
-			public const int espacio5251 = 2131034166;
-			
-			// aapt resource value: 0x7f050035
-			public const int espacio5352 = 2131034165;
-			
-			// aapt resource value: 0x7f050034
-			public const int espacio5453 = 2131034164;
-			
-			// aapt resource value: 0x7f050033
-			public const int espacio5554 = 2131034163;
+			public const int espacio4847 = 2131034200;
 			
 			// aapt resource value: 0x7f050038
-			public const int espacio6162 = 2131034168;
+			public const int espacio5161 = 2131034168;
+			
+			// aapt resource value: 0x7f050037
+			public const int espacio5251 = 2131034167;
+			
+			// aapt resource value: 0x7f050036
+			public const int espacio5352 = 2131034166;
+			
+			// aapt resource value: 0x7f050035
+			public const int espacio5453 = 2131034165;
+			
+			// aapt resource value: 0x7f050034
+			public const int espacio5554 = 2131034164;
 			
 			// aapt resource value: 0x7f050039
-			public const int espacio6263 = 2131034169;
+			public const int espacio6162 = 2131034169;
 			
 			// aapt resource value: 0x7f05003a
-			public const int espacio6364 = 2131034170;
+			public const int espacio6263 = 2131034170;
 			
 			// aapt resource value: 0x7f05003b
-			public const int espacio6465 = 2131034171;
+			public const int espacio6364 = 2131034171;
 			
-			// aapt resource value: 0x7f050044
-			public const int espacio7172 = 2131034180;
-			
-			// aapt resource value: 0x7f050043
-			public const int espacio7181 = 2131034179;
-			
-			// aapt resource value: 0x7f050046
-			public const int espacio7273 = 2131034182;
-			
-			// aapt resource value: 0x7f050047
-			public const int espacio7374 = 2131034183;
-			
-			// aapt resource value: 0x7f050048
-			public const int espacio7475 = 2131034184;
+			// aapt resource value: 0x7f05003c
+			public const int espacio6465 = 2131034172;
 			
 			// aapt resource value: 0x7f050045
-			public const int espacio8171 = 2131034181;
+			public const int espacio7172 = 2131034181;
+			
+			// aapt resource value: 0x7f050044
+			public const int espacio7181 = 2131034180;
+			
+			// aapt resource value: 0x7f050047
+			public const int espacio7273 = 2131034183;
+			
+			// aapt resource value: 0x7f050048
+			public const int espacio7374 = 2131034184;
+			
+			// aapt resource value: 0x7f050049
+			public const int espacio7475 = 2131034185;
+			
+			// aapt resource value: 0x7f050046
+			public const int espacio8171 = 2131034182;
+			
+			// aapt resource value: 0x7f050043
+			public const int espacio8281 = 2131034179;
 			
 			// aapt resource value: 0x7f050042
-			public const int espacio8281 = 2131034178;
+			public const int espacio8382 = 2131034178;
 			
 			// aapt resource value: 0x7f050041
-			public const int espacio8382 = 2131034177;
+			public const int espacio8483 = 2131034177;
 			
 			// aapt resource value: 0x7f050040
-			public const int espacio8483 = 2131034176;
+			public const int espacio8584 = 2131034176;
 			
-			// aapt resource value: 0x7f05003f
-			public const int espacio8584 = 2131034175;
+			// aapt resource value: 0x7f050056
+			public const int espacioEntreDentalInferior = 2131034198;
 			
-			// aapt resource value: 0x7f050054
-			public const int espacioEntreDentalInferior = 2131034196;
-			
-			// aapt resource value: 0x7f05003d
-			public const int espacioEntreDentalInferiorTemporal = 2131034173;
+			// aapt resource value: 0x7f05003e
+			public const int espacioEntreDentalInferiorTemporal = 2131034174;
 			
 			// aapt resource value: 0x7f050005
 			public const int espacioEntreDentalSuperior = 2131034117;
@@ -291,119 +297,119 @@ namespace DentProy.Droid
 			// aapt resource value: 0x7f050015
 			public const int pieza28 = 2131034133;
 			
-			// aapt resource value: 0x7f05006d
-			public const int pieza31 = 2131034221;
-			
-			// aapt resource value: 0x7f05006e
-			public const int pieza32 = 2131034222;
-			
 			// aapt resource value: 0x7f05006f
-			public const int pieza33 = 2131034223;
+			public const int pieza31 = 2131034223;
 			
 			// aapt resource value: 0x7f050070
-			public const int pieza34 = 2131034224;
+			public const int pieza32 = 2131034224;
 			
 			// aapt resource value: 0x7f050071
-			public const int pieza35 = 2131034225;
+			public const int pieza33 = 2131034225;
 			
 			// aapt resource value: 0x7f050072
-			public const int pieza36 = 2131034226;
+			public const int pieza34 = 2131034226;
 			
 			// aapt resource value: 0x7f050073
-			public const int pieza37 = 2131034227;
+			public const int pieza35 = 2131034227;
 			
 			// aapt resource value: 0x7f050074
-			public const int pieza38 = 2131034228;
+			public const int pieza36 = 2131034228;
+			
+			// aapt resource value: 0x7f050075
+			public const int pieza37 = 2131034229;
+			
+			// aapt resource value: 0x7f050076
+			public const int pieza38 = 2131034230;
+			
+			// aapt resource value: 0x7f05006e
+			public const int pieza41 = 2131034222;
+			
+			// aapt resource value: 0x7f05006d
+			public const int pieza42 = 2131034221;
 			
 			// aapt resource value: 0x7f05006c
-			public const int pieza41 = 2131034220;
+			public const int pieza43 = 2131034220;
 			
 			// aapt resource value: 0x7f05006b
-			public const int pieza42 = 2131034219;
+			public const int pieza44 = 2131034219;
 			
 			// aapt resource value: 0x7f05006a
-			public const int pieza43 = 2131034218;
+			public const int pieza45 = 2131034218;
 			
 			// aapt resource value: 0x7f050069
-			public const int pieza44 = 2131034217;
+			public const int pieza46 = 2131034217;
 			
 			// aapt resource value: 0x7f050068
-			public const int pieza45 = 2131034216;
+			public const int pieza47 = 2131034216;
 			
 			// aapt resource value: 0x7f050067
-			public const int pieza46 = 2131034215;
-			
-			// aapt resource value: 0x7f050066
-			public const int pieza47 = 2131034214;
-			
-			// aapt resource value: 0x7f050065
-			public const int pieza48 = 2131034213;
-			
-			// aapt resource value: 0x7f05002c
-			public const int pieza51 = 2131034156;
-			
-			// aapt resource value: 0x7f05002b
-			public const int pieza52 = 2131034155;
-			
-			// aapt resource value: 0x7f05002a
-			public const int pieza53 = 2131034154;
-			
-			// aapt resource value: 0x7f050029
-			public const int pieza54 = 2131034153;
-			
-			// aapt resource value: 0x7f050028
-			public const int pieza55 = 2131034152;
+			public const int pieza48 = 2131034215;
 			
 			// aapt resource value: 0x7f05002d
-			public const int pieza61 = 2131034157;
+			public const int pieza51 = 2131034157;
+			
+			// aapt resource value: 0x7f05002c
+			public const int pieza52 = 2131034156;
+			
+			// aapt resource value: 0x7f05002b
+			public const int pieza53 = 2131034155;
+			
+			// aapt resource value: 0x7f05002a
+			public const int pieza54 = 2131034154;
+			
+			// aapt resource value: 0x7f050029
+			public const int pieza55 = 2131034153;
 			
 			// aapt resource value: 0x7f05002e
-			public const int pieza62 = 2131034158;
+			public const int pieza61 = 2131034158;
 			
 			// aapt resource value: 0x7f05002f
-			public const int pieza63 = 2131034159;
+			public const int pieza62 = 2131034159;
 			
 			// aapt resource value: 0x7f050030
-			public const int pieza64 = 2131034160;
+			public const int pieza63 = 2131034160;
 			
 			// aapt resource value: 0x7f050031
-			public const int pieza65 = 2131034161;
+			public const int pieza64 = 2131034161;
 			
-			// aapt resource value: 0x7f05004e
-			public const int pieza71 = 2131034190;
-			
-			// aapt resource value: 0x7f05004f
-			public const int pieza72 = 2131034191;
+			// aapt resource value: 0x7f050032
+			public const int pieza65 = 2131034162;
 			
 			// aapt resource value: 0x7f050050
-			public const int pieza73 = 2131034192;
+			public const int pieza71 = 2131034192;
 			
 			// aapt resource value: 0x7f050051
-			public const int pieza74 = 2131034193;
+			public const int pieza72 = 2131034193;
 			
 			// aapt resource value: 0x7f050052
-			public const int pieza75 = 2131034194;
-			
-			// aapt resource value: 0x7f05004d
-			public const int pieza81 = 2131034189;
-			
-			// aapt resource value: 0x7f05004c
-			public const int pieza82 = 2131034188;
-			
-			// aapt resource value: 0x7f05004b
-			public const int pieza83 = 2131034187;
-			
-			// aapt resource value: 0x7f05004a
-			public const int pieza84 = 2131034186;
-			
-			// aapt resource value: 0x7f050049
-			public const int pieza85 = 2131034185;
+			public const int pieza73 = 2131034194;
 			
 			// aapt resource value: 0x7f050053
-			public const int underJaw = 2131034195;
+			public const int pieza74 = 2131034195;
 			
-			// aapt resource value: 0x7f05003c
-			public const int underTempJaw = 2131034172;
+			// aapt resource value: 0x7f050054
+			public const int pieza75 = 2131034196;
+			
+			// aapt resource value: 0x7f05004f
+			public const int pieza81 = 2131034191;
+			
+			// aapt resource value: 0x7f05004e
+			public const int pieza82 = 2131034190;
+			
+			// aapt resource value: 0x7f05004d
+			public const int pieza83 = 2131034189;
+			
+			// aapt resource value: 0x7f05004c
+			public const int pieza84 = 2131034188;
+			
+			// aapt resource value: 0x7f05004b
+			public const int pieza85 = 2131034187;
+			
+			// aapt resource value: 0x7f050055
+			public const int underJaw = 2131034197;
+			
+			// aapt resource value: 0x7f05003d
+			public const int underTempJaw = 2131034173;
 			
 			// aapt resource value: 0x7f050004
 			public const int upperJaw = 2131034116;
@@ -428,7 +434,10 @@ namespace DentProy.Droid
 			public const int pieza13 = 2131099648;
 			
 			// aapt resource value: 0x7f060001
-			public const int pieza83 = 2131099649;
+			public const int pieza53 = 2131099649;
+			
+			// aapt resource value: 0x7f060002
+			public const int pieza83 = 2131099650;
 			
 			static Idi()
 			{
